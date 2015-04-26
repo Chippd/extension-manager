69c75d5b40cab77076bf01e41d2a4523bba73aeb
Chris Dermody <chrisdermody1@gmail.com>
2015-04-26 19:30:26 +0100

    just some minor code indenting, was reviewing code with maguire

--- myscript.js
+++ myscript.js
@@ -59,51 +59,50 @@ $(document).ready(function(){
       })
 
       extArray.forEach(function(entry) { //cycle through each entry
-      // console.log(entry);
+        // console.log(entry);
 
-      //push each extension name to an array (nameArray) - no reason as of yet....
-      nameArray.push(entry.name);
+        //push each extension name to an array (nameArray) - no reason as of yet....
+        nameArray.push(entry.name);
 
 
-      //get current state of ext (if it's on or off) and apply that to the checkbox. also changing "true" to 'on' and 'false' to 'off'
-      state = entry.enabled;
-      if(state){
-        state = 'on';
-        entry.checkbox = "checked";
-      } else {
-        state = 'off';
-        entry.checkbox = '';
-      }
-      console.log("state is: "+state);
+        //get current state of ext (if it's on or off) and apply that to the checkbox. also changing "true" to 'on' and 'false' to 'off'
+        state = entry.enabled;
+        if(state){
+          state = 'on';
+          entry.checkbox = "checked";
+        } else {
+          state = 'off';
+          entry.checkbox = '';
+        }
+        console.log("state is: "+state);
 
-      
-      //extension icons are stored in an array (entry.icons), but not all extensions have icons.
-      if(entry.icons === undefined){
-        imgsrc = 'icon-128.png'  //and if there aren't any icons, we set a default
-      }else {
-        //and if there are indeed an array of icons, we want to take the
-        //highest res one (which is going to be the last one in the array)
-        //so we check for the array length, and then use that value (-1) to get the array item we want
-        //then we set that item's url as our app icon url
-        var iconCount = entry.icons.length; 
-        console.log('number of icons found: '+iconCount);
-        imgsrc = entry.icons[iconCount-1].url;
-      }
+        
+        //extension icons are stored in an array (entry.icons), but not all extensions have icons.
+        if(entry.icons === undefined){
+          imgsrc = 'icon-128.png'  //and if there aren't any icons, we set a default
+        }else {
+          //and if there are indeed an array of icons, we want to take the
+          //highest res one (which is going to be the last one in the array)
+          //so we check for the array length, and then use that value (-1) to get the array item we want
+          //then we set that item's url as our app icon url
+          var iconCount = entry.icons.length; 
+          console.log('number of icons found: '+iconCount);
+          imgsrc = entry.icons[iconCount-1].url;
+        }
 
-      entry.pic = imgsrc; //setting the url we got earlier as the entry.pic
-      entry.state = state; //setting the state we got earlier as the entry.state
+        entry.pic = imgsrc; //setting the url we got earlier as the entry.pic
+        entry.state = state; //setting the state we got earlier as the entry.state
 
-      console.log('appId is: '+entry.id); //for debugging
+        console.log('appId is: '+entry.id); //for debugging
 
-      console.log('imgsrc is: '+imgsrc); //for debugging
+        console.log('imgsrc is: '+imgsrc); //for debugging
 
-      $('#extensions').append(template(entry)); //append each of our entries to our template to the #extensions div. 
-    });
+        $('#extensions').append(template(entry)); //append each of our entries to our template to the #extensions div. 
+      });
 
       checkboxListener(); //run the function which listens for a change in a checkbox state
       // console.log(nameArray); -- was console loggin names for search feature
     });
-
 
 
 
